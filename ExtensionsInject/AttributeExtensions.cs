237517a917281ef58df984386192c28b2f8a9060
dust <net_code@163.com>
2020-06-21 21:40:30 +0800

    made changes

---
+++ ExtensionsInject/AttributeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExtensionsInject
+{
+    public class Transient : Attribute
+    {
+    }
+
+    public class Singleton : Attribute
+    {
+    }
+
+    public class Scoped : Attribute
+    {
+    }
+}
---
+++ ExtensionsInject/ServiceInjectExtensions.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ExtensionsInject
+{
+    public static class ServiceInjectExtensions
+    {
+        public static void AddTransientList(this IServiceCollection services, System.Reflection.Assembly assembly)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+            foreach (Type item in assembly.GetTypes())
+            {
+                if (item.GetCustomAttributes(typeof(Transient)).Count() > 0)
+                {
+                    if (!item.IsInterface)
+                    {
+                        services.AddTransient(item.GetInterfaces()[0], item);
+                    }
+                }
+            }
+        }
+
+        public static void AddScopedList(this IServiceCollection services, System.Reflection.Assembly assembly)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+            foreach (Type item in assembly.GetTypes())
+            {
+                if (item.GetCustomAttributes(typeof(Scoped)).Count() > 0)
+                {
+                    if (!item.IsInterface)
+                    {
+                        services.AddTransient(item.GetInterfaces()[0], item);
+                    }
+                }
+            }
+        }
+
+        public static void AddSingletonList(this IServiceCollection services, System.Reflection.Assembly assembly)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+            foreach (Type item in assembly.GetTypes())
+            {
+                if (item.GetCustomAttributes(typeof(Singleton)).Count() > 0)
+                {
+                    if (!item.IsInterface)
+                    {
+                        services.AddTransient(item.GetInterfaces()[0], item);
+                    }
+                }
+            }
+        }
+    }
+}
